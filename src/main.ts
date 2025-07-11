207503bd026f0dfeec75e896f9a9057d5d5d8659
Liferoijrm <pedroextrer@gmail.com>
2025-07-10 23:14:43 -0300

    :warning: view: adiciona visualização de erros no terminal

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common'
+import { BadRequestException } from '@nestjs/common/exceptions/bad-request.exception';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors(); 
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true,
+    errorHttpStatusCode: 400,
+    // mostra erros no console
+    exceptionFactory: (errors) => {
+      console.error('Erro de validação:', errors);
+      return new BadRequestException(errors);
+    },
+  }));
   await app.listen(3001);
 }
 bootstrap();
